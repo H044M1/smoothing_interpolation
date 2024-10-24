40d9ffbf219a4b754c94b7bd6bf7579de0c9000e
Anton <116296089+H044M1@users.noreply.github.com>
2024-10-24 20:42:20 +0700

    removing typos

--- SaveResults.cpp
+++ SaveResults.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 void save_to_file(const vector<double>& values_y, const vector<double>& interpolated,
-    const vector<double>& smoothed0, const vector<double>& smoothed4,
-    const vector<double>& smoothed8, const vector<double>& smoothed99,
+    const vector<double>& smoothed0, const vector<double>& smoothed33,
+    const vector<double>& smoothed66, const vector<double>& smoothed99,
     const string& filename) {
     ofstream file(filename);
     if (file.is_open()) {
         for (size_t i = 0; i < values_y.size(); ++i) {
             file << values_y[i] << " " << interpolated[i] << " " << smoothed0[i]
-                << " " << smoothed4[i] << " " << smoothed8[i] << " " << smoothed99[i] << "\n";
+                << " " << smoothed33[i] << " " << smoothed66[i] << " " << smoothed99[i] << "\n";
         }
         file.close();
     }
